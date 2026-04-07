abd720f644a19e30451a930e917455ea10f0b5ba
nik <rafseno78rush@gmail.com>
2026-04-07 11:24:12 +0800

    fixed the laravel repository structure to be compatible with electron and inertiajs.

---
+++ syllabus/resources/js/Pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>PUP SRC Syllabus Generator</h1>
+        </div>
+    );
+}
---
+++ syllabus/resources/js/app.tsx
@@ -0,0 +1,10 @@
+import './css/app.css';
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+
+createInertiaApp({
+    resolve: name => import(`./Pages/${name}.tsx`).then(module => module.default),
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />);
+    },
+});
